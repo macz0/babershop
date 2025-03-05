7040402514667e705459f21684e17bce077e5858
John Doe <john.doe@contoso.com>
2025-03-05 13:34:08 -0500

    se actualiza

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,15 @@ const PORT = process.env.PORT ?? 3008;
 
 
 const main = async () => {
-    const Flow = createFlow([menu])
-    const adapterDB = new Database()
 
     const { handleCtx, httpServer } = await createBot({
-        flow: Flow,
-        provider: createProvider(Provider, {
+        database: new Database(),
+        flow: createFlow([menu]),
+        provider: createProvider(Provider,  {
             jwtToken: process.env.jwtToken,
             numberId: process.env.numberId,
             verifyToken: process.env.verifyToken,
             version: 'v22.0',
-            database: adapterDB
         })
     })
     httpServer(+PORT)
